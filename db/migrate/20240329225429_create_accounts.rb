a53eb5c8c074d5c7e03274bc7f0f758b5fabbc2c
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2024-03-30 07:55:27 +0900

    not null

--- db/migrate/20240329225429_create_accounts.rb
+++ db/migrate/20240329225429_create_accounts.rb
@@ -1,7 +1,7 @@
 class CreateAccounts < ActiveRecord::Migration[7.1]
   def change
     create_table :accounts do |t|
-      t.string :email
+      t.string :email, null: false
 
       t.timestamps
     end
--- db/migrate/20240329225449_create_profiles.rb
+++ db/migrate/20240329225449_create_profiles.rb
@@ -2,8 +2,8 @@ class CreateProfiles < ActiveRecord::Migration[7.1]
   def change
     create_table :profiles do |t|
       t.references :account, index: {:unique=>true}, null: false, foreign_key: true
-      t.string :name
-      t.integer :age
+      t.string :name, null: false
+      t.integer :age, null: false
 
       t.timestamps
     end
