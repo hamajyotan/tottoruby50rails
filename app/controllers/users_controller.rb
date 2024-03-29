4a3e6dae948914e098114126a0b36722946bfa7b
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2024-03-30 07:59:54 +0900

    User model finding

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
+    @users = Account.includes(:profile).all.map { User.new(_1) }
   end
 
   # GET /users/1
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params[:id])
+      @user = User.new(Account.find(params[:id]))
     end
 
     def set_new_user
