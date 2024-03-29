9e27e7d53b059a2f9ce3ba92b215cb670e0dbdb0
SAKAGUCHI Takashi <hamajyotan@gmail.com>
2024-03-30 07:58:27 +0900

    change user definition to composed-model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
-class User < ApplicationRecord
+class User < ActiveRecordCompose::Model
+  def initialize(account = Account.new)
+    super({})
+    @account = account
+    @profile = account.then { _1.profile || _1.build_profile }
+    models << account << profile
+  end
+
+  private
+
+  attr_reader :account, :profile
+
+  delegate_attribute :email, to: :account
+  delegate_attribute :name, :age, to: :profile
 end
